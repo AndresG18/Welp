060cfd01aed28e8eb514d696f1b95c562605fd56
Chris Kang <chriskangfamily@gmail.com>
2024-05-21 10:51:12 -0700

    Update

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,22 +1,24 @@
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+import LoginFormPage from '../components/LoginFormPage';
+import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
 
-
-export const router = () => {
-  return (
-    <BrowserRouter>
-    <Layout/>
-      <Routes>
-        <Route path="/" element={ <h1>Welcome!</h1> }>
-          <Route path="/businesses" element={<null />} />
-          <Route path="/businesses/new" element={<null />} />
-          <Route path="/businesses/:busId" element={<null />} />
-          <Route path="/businesses/:id/reviews" element={<null />} />
-          <Route path="/businesses/:id/reviews/new" element={<null />} />
-          <Route path="/reviews/:id/edit" element={<null />} />
-          <Route path='*' element={<h1>Page not Found</h1>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+export const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <h1>Welcome!</h1>,
+      },
+      {
+        path: "login",
+        element: <LoginFormPage />,
+      },
+      {
+        path: "signup",
+        element: <SignupFormPage />,
+      },
+    ],
+  },
+]);
