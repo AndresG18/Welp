02dedea115b3321e9a61f978e0cb8aa4185cde4d
Chris Kang <chriskangfamily@gmail.com>
2024-05-21 12:55:20 -0700

    Frontend Route Updates

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
+import HomePage from '../components/HomePage/HomePage'
+import Businesses from '../components/Businesses/Businesses';
+import OneBusiness from '../components/OneBusiness/OneBusiness'
+import BusinessForm from '../components/BusinessForm/BusinessForm'
+import ReviewsForm from '../components/ReviewsForm/ReviewForm'
 
 export const router = createBrowserRouter([
   {
@@ -9,7 +14,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
+        element: <HomePage />,
       },
       {
         path: "login",
@@ -19,6 +24,34 @@ export const router = createBrowserRouter([
         path: "signup",
         element: <SignupFormPage />,
       },
+      {
+        path: "/bus",
+        element: <Businesses />
+      },
+      {
+        path: "/bus/:busId",
+        element: <OneBusiness />
+      },
+      {
+        path: "/bus/new",
+        element: <BusinessForm />
+      },
+      {
+        path: "/bus/:busId/edit",
+        element: <BusinessForm />
+      },
+      {
+        path: "bus/:busId/reviews/new",
+        element: <ReviewsForm />
+      },
+      {
+        path: "bus/:busId/reviews/:reviewId/edit",
+        element: <ReviewsForm />
+      },
+      {
+        path: "*",
+        element: <h1>Not Found</h1>
+      }
     ],
   },
 ]);
