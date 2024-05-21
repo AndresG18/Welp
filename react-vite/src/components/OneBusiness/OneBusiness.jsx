0680783984d770bb7edb23597969e8d8a62da4b7
Daniel <daniel.choi91011@gmail.com>
2024-05-21 13:03:56 -0700

    onebusiness

--- react-vite/src/components/OneBusiness/OneBusiness.jsx
+++ react-vite/src/components/OneBusiness/OneBusiness.jsx
@@ -1,6 +1,124 @@
+import {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {getBusinessByIdThunk} from '../../redux/business';            // check var/func name
+import {getReviewsByBusinessIdThunk} from '../../redux/reviews';           // check var/func name
+import {useModal} from "../../context/Modal";               // optional modals???
+// import DeleteReviewModal from "../DeleteReviewModal";
+// import OpenModalButton from '../OpenModalButton';
 
-export default function OneBusiness() {
-  return (
-    <div>OneBusiness</div>
-  )
+function OneBusiness(){
+    const [isLoaded, setIsLoaded] = useState(false);
+    const dispatch = useDispatch();
+    const {busId} = useParams();
+    const bus = useSelector(state => state.business);       // var names, as well as nested object path. do the same for following lines
+    const reviews = useSelector(state => state.reviews);            
+    const sessionUser = useSelector(state => state.session.user);
+
+    const {setModalContent} = useModal();
+
+    console.log('Business from useSelector-----> ', bus)
+    console.log('TEST-----------------------')
+
+    useEffect(() => {
+        async function getBusData(){
+           await dispatch(getBusinessByIdThunk(busId));
+           await dispatch(getReviewsByBusinessIdThunk(busId));
+           setIsLoaded(true);
+        }
+        getBusData();
+    }, [dispatch, busId, isLoaded]);
+
+    // const reserveClick = () => {
+    //     alert('Feature coming soon')
+    // }
+
+    return(
+        <div>
+            {isLoaded && bus &&
+            
+                <div id='busbyid'>
+                    <div className='bus-images-bar'>
+                        <img className='bus-main-pic' style={{height: '600px', width: '500px'}}src={bus.BusinessImages[0]?.url} alt="main-pic"/>
+                        <img className="bus-quad-pic" style={{height: '250px', width: 'auto'}} src={bus.BusinessImages[1]?.url || '/chicken-no-img.jpg'} alt="small-1" />
+                        <img className="bus-quad-pic" style={{height: '250px', width: 'auto'}} src={bus.BusinessImages[2]?.url || '/chicken-no-img.jpg'} alt="small-1" />
+                        <img className="bus-quad-pic" style={{height: '250px', width: 'auto'}} src={bus.BusinessImages[3]?.url || '/chicken-no-img.jpg'} alt="small-1" />
+                    </div>
+
+                    <div className="bus-title-block">
+                        <h1 className="bus-name" style={{fontSize: '40px'}}>{bus.name}</h1>
+                        <p className="bus-star-reviews">{bus.avgStarRating} ({bus.totalreviews})</p>
+                        <p className="bus-hours">{bus.hours}</p>
+                    </div>
+
+                    <div className="bus-page-lower">
+                        <div className="bus-page-lower-left">
+                            <p className="bus-host" style={{fontSize: '28px'}}>Hosted by {bus.Owner.firstName} {bus.Owner.lastName}</p>
+                            <p className="bus-description" style={{fontSize: '25px'}}>Features: {bus.description}</p>
+
+                            {!bus.numReviews ? <p style={{fontSize: '25px'}}>New</p> : 
+                            bus.numReviews === 1 ? <p className="det-rev-sum" style={{fontSize: '25px'}}><img className="details-black-star" src='/black-star.jpg'/>
+                            {bus.avgStarRating.toFixed(1)} &#183; {bus.numReviews} Review</p> :
+                            <p className="det-rev-sum" style={{fontSize: '25px'}}><img className='details-black-star' src='/black-star.jpg'/> 
+                            {bus.avgStarRating.toFixed(1)} &#183; {bus.numReviews} Reviews</p>}
+
+                            <div className='bus-review-list'>
+                                {!reviews.length && sessionUser && sessionUser.id !== bus.Owner.id ? 
+                                (
+                                <>
+                                    <p style={{fontSize: '25px'}}>Be the first to post a review!</p>
+                                    <button onClick={handleReviewModal} style={{fontSize: '25px'}}>Post Your Review</button>
+                                </>
+                                ) : (
+                                <>
+                                    <p className="bus-reviews-subtitle" style={{fontSize: '25px'}}>Reviews:</p>
+                                    {sessionUser && sessionUser.id !== bus.Owner.id && !reviews.find(obj => obj.userId === sessionUser.id) && 
+                                    (<button onClick={handleReviewModal} style={{fontSize: '25px'}}>Post Your Review</button>)}
+                                </>
+                                )}
+                                
+                                {
+                                reviews.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                                .map((review, index) => (
+                                    <div key={index}>
+                                        <p style={{fontSize: '25px'}}>Reviewer: {review.User.firstName}</p>
+                                        <p style={{fontSize: '25px'}}>Date of review: {new Date(review.createdAt).toLocaleString("default", { month: "long", year: "numeric" })}</p>
+                                        <p style={{fontSize: '25px'}}>Comments: {review.review}</p>
+                                        <p style={{fontSize: '25px'}}>Rating: {review.stars}</p>
+                                        
+                                        {/* {sessionUser && review.userId === sessionUser.id && 
+                                        (<OpenModalButton className='delete-bus' buttonText='Delete' modalComponent={<DeleteReviewModal busId={busId} reviewId={review.id}/>}/>)} */}
+                                        
+                                        <p>____________________________________________________</p>
+                                    </div>
+                                ))}
+                          
+                            </div>
+                        </div>
+    
+                        <div className="bus-page-lower-right">
+                            <div className='callout-box'>
+                                <div className="callout-box-upper"><h2 style={{fontSize: '40px'}}>Don&apos;t miss out!</h2></div>
+                                <div className='callout-box-mid'>
+                                    <div className="callout-right-text">
+                                        {!bus.numReviews ? <p className="callout-review-group" style={{fontSize: '30px'}}>New</p> : 
+                                        bus.numReviews === 1 ? (<p className="callout-review-group" style={{fontSize: '30px'}}><img className="callout-black-star" src='/black-star.jpg'/>
+                                        {bus.avgStarRating.toFixed(1)} &#183; {bus.numReviews} Review</p>) : 
+                                        <p className="callout-review-group" style={{fontSize: '30px'}}>
+                                            <img className="callout-black-star" src='/black-star.jpg'/>
+                                            {bus.avgStarRating.toFixed(1)} &#183; {bus.numReviews} Reviews
+                                        </p>}
+                                    </div>
+                                    <p className="callout-right-price" style={{fontSize: '30px'}}>{`$${bus.price} / night`}</p>
+                                </div>
+                                <button className='callout-box-reserve' onClick={reserveClick} style={{fontSize: '30px'}}>Reserve</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
+    )
 }
+
+export default OneBusiness;
