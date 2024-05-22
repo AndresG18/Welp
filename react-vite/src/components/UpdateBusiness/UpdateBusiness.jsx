048cbabef6fe26534e319290daba0c5d1093d670
Chris Kang <chriskangfamily@gmail.com>
2024-05-22 14:45:13 -0700

    Update Business

--- react-vite/src/components/UpdateBusiness/UpdateBusiness.jsx
+++ react-vite/src/components/UpdateBusiness/UpdateBusiness.jsx
@@ -1,9 +1,16 @@
 import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
 import BusinessForm from "../BusinessForm/BusinessForm"
 
 export default function UpdateBusiness() {
-    const bus = useSelector(state => state.business.business)
-    console.log(bus)
+  const bus = useSelector(state => state.business.business)
+  const navigate = useNavigate()
+  const { busId } = useParams()
+
+  useEffect(() => {
+    if (!bus) navigate(`/bus/${busId}`)
+  })
   return (
     <BusinessForm bus={bus.business} />
   )
