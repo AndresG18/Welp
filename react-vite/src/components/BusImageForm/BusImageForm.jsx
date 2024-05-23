59d3596fff79ea3714ab213c501d3c780336c5ba
Chris Kang <chriskangfamily@gmail.com>
2024-05-22 18:21:10 -0700

    Update to Manage Business

--- react-vite/src/components/BusImageForm/BusImageForm.jsx
+++ react-vite/src/components/BusImageForm/BusImageForm.jsx
@@ -1,10 +1,10 @@
-import  { useState, useEffect } from 'react';
-import { useNavigate , useParams} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { createImageThunk } from '../../redux/image';
 import { getBusinessByIdThunk } from '../../redux/business';
 
-const BusinessImageForm = ( ) => {
+const BusinessImageForm = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [url, setUrl] = useState('');
@@ -12,25 +12,25 @@ const BusinessImageForm = ( ) => {
 
     const user = useSelector(state => state.session.user);
     const business = useSelector(state => state?.business?.business?.business)
-    const {busId} = useParams()
+    const { busId } = useParams()
     console.log(business)
-    
+
     useEffect(() => {
         dispatch(getBusinessByIdThunk(busId))
-        if (!user ) navigate('/');
-    }, [user, navigate,dispatch]);
+        if (!user) navigate('/');
+    }, [user, navigate, dispatch, busId]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setErrors({});
-        
+
         const imageObj = { url };
         const data = await dispatch(createImageThunk(busId, imageObj));
 
         if (data.errors || data.message) {
             setErrors(data?.errors);
         } else {
-            setUrl(''); 
+            setUrl('');
             navigate(`/bus/${busId}`);
         }
     };
@@ -41,13 +41,13 @@ const BusinessImageForm = ( ) => {
             <form onSubmit={handleSubmit}>
                 <section className='form-section'>
                     <h3>Image URL</h3>
-                    <input 
-                        type='text' 
-                        value={url} 
-                        onChange={(e) => setUrl(e.target.value)} 
-                        placeholder='Enter image URL' 
+                    <input
+                        type='text'
+                        value={url}
+                        onChange={(e) => setUrl(e.target.value)}
+                        placeholder='Enter image URL'
                     />
-                {errors?.url && <p className="error">{errors?.url}</p>}
+                    {errors?.url && <p className="error">{errors?.url}</p>}
                 </section>
                 <button type='submit'>Add Image</button>
             </form>
