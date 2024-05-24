621865fe7ce6078f61d3246e55709042329bb1bd
Chris Kang <chriskangfamily@gmail.com>
2024-05-24 15:57:02 -0700

    Main Bus Page Styled

--- react-vite/src/components/Businesses/AllBusinesses.jsx
+++ react-vite/src/components/Businesses/AllBusinesses.jsx
@@ -3,6 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllBusinessesThunk } from "../../redux/businesses";
 import Businesses from "./Businesses";
 import { FaSearch } from "react-icons/fa";
+import { FaBreadSlice } from "react-icons/fa";
+import { FaCoffee } from "react-icons/fa";
+import { FaStore } from "react-icons/fa";
+import { FaBowlFood } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa";
+
 import "./Businesses.css";
 
 function AllBusinesses() {
@@ -15,7 +22,7 @@ function AllBusinesses() {
   const [price, setPrice] = useState('')
   const [rating, setRating] = useState('')
   // const [page, setPage] = useState(1);
-  // const [size, setSize] = useState(4); 
+  // const [size, setSize] = useState(4);
 
   // const fetchPage = async (page, size) => {
   //   const res = await fetch(`/api/bus?page=${page}&size=${size}`)
@@ -38,11 +45,11 @@ function AllBusinesses() {
     setFilteredList(list);
   }, [categoryId, rating, price, businessList]);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (searched.trim() == '') setFilteredList(businessList)
-  },[searched,businessList])
+  }, [searched, businessList])
 
- 
+
   useEffect(() => {
     dispatch(getAllBusinessesThunk())//add page and size if paginating
       .then(() => {
@@ -88,7 +95,7 @@ function AllBusinesses() {
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-     handleSearch();
+      handleSearch();
     }
   };
 
@@ -101,8 +108,11 @@ function AllBusinesses() {
         onChange={(e) => setSearched(e.target.value)}
         onKeyDown={handleKeyPress}
         placeholder="Search for businesses"
+        id="search-bar-itself"
       />
-      <FaSearch onClick={handleSearch} />
+      <div id="search-icon">
+        <FaSearch id="search-icon-itself" onClick={handleSearch} />
+      </div>
     </div>
   );
 
@@ -114,11 +124,11 @@ function AllBusinesses() {
       </div>
       <div className="container-filter-bus">
         <div className="filter-container">
-          <p className="filter-title">Filters</p>
+          <p className="filter-title-main">Filters</p>
           <div className="filters category">
-            <p>Categories :</p>
+            <p className="filter-title">Categories :</p>
             <div className="name-val">
-              <p>Bakery</p>
+              <p className="filter-title-icon">Bakery <FaBreadSlice /></p>
               <input
                 type="checkbox"
                 value={1}
@@ -127,7 +137,7 @@ function AllBusinesses() {
               />
             </div>
             <div className="name-val">
-              <p>Restaurant</p>
+              <p className="filter-title-icon">Restaurant <FaBowlFood /></p>
               <input
                 type="checkbox"
                 value={2}
@@ -136,7 +146,7 @@ function AllBusinesses() {
               />
             </div>
             <div className="name-val">
-              <p>Cafe</p>
+              <p className="filter-title-icon">Cafe <FaCoffee /></p>
               <input
                 type="checkbox"
                 value={3}
@@ -145,7 +155,7 @@ function AllBusinesses() {
               />
             </div>
             <div className="name-val">
-              <p>Store</p>
+              <p className="filter-title-icon">Store <FaStore /></p>
               <input
                 type="checkbox"
                 value={4}
@@ -155,9 +165,9 @@ function AllBusinesses() {
             </div>
           </div>
           <div className="filters price">
-            <p>Price Range  :</p>
+            <p className="filter-title">Price Range  :</p>
             <div className="name-val">
-              <p>$ (0 - 15 USD)</p>
+              <p className="filter-title-icon">(0 - 15 USD) <span className="span-dollar">$</span></p>
               <input
                 type="checkbox"
                 value="Low"
@@ -166,7 +176,7 @@ function AllBusinesses() {
               />
             </div>
             <div className="name-val">
-              <p>$$ (15 - 40 USD)</p>
+              <p className="filter-title-icon">(15 - 40 USD <span className="span-dollar">$$</span></p>
               <input
                 type="checkbox"
                 value="Medium"
@@ -175,7 +185,7 @@ function AllBusinesses() {
               />
             </div>
             <div className="name-val">
-              <p>$$$ (40+ USD)</p>
+              <p className="filter-title-icon"> (40+ USD) <span className="span-dollar">$$$</span></p>
               <input
                 type="checkbox"
                 value="High"
@@ -185,9 +195,9 @@ function AllBusinesses() {
             </div>
           </div>
           <div className="filters rating">
-            <p>Star Rating :</p>
+            <p className="filter-title">Star Rating :</p>
             <div className="name-val">
-              <p>1 Star & Up</p>
+              <p className="filter-title-icon">1 Star & Up <FaStar style={{ color: "#B90000" }} /></p>
               <input
                 type="checkbox"
                 value={1}
@@ -196,7 +206,7 @@ function AllBusinesses() {
               />
             </div>
             <div className="name-val">
-              <p>2 Stars & Up</p>
+              <p className="filter-title-icon">2 Stars & Up <FaStar style={{ color: "#B95F00" }} /></p>
               <input
                 type="checkbox"
                 value={2}
@@ -205,7 +215,7 @@ function AllBusinesses() {
               />
             </div>
             <div className="name-val">
-              <p>3 Stars & Up</p>
+              <p className="filter-title-icon">3 Stars & Up <FaStar style={{ color: "#8A9300" }} /></p>
               <input
                 type="checkbox"
                 value={3}
@@ -214,7 +224,7 @@ function AllBusinesses() {
               />
             </div>
             <div className="name-val">
-              <p>4 Stars & Up</p>
+              <p className="filter-title-icon">4 Stars & Up <FaStar style={{ color: "green" }} /></p>
               <input
                 type="checkbox"
                 value={4}
@@ -225,15 +235,15 @@ function AllBusinesses() {
           </div>
         </div>
         <div className="bus-container">
-          <p>All Results</p>
-          { filteredList.length > 0 ? filteredList.map(business => (
+          <p id="all-results">All Results</p>
+          {filteredList.length > 0 ? filteredList.map(business => (
             <Businesses key={business.id} business={business} />
           )) : <h1> No Results Found</h1>}
           {/* <div className="pagination-controls">
             <button onClick={previousPage} disabled={page === 1}>Previous</button> PAGGGINATIONNN
             <span>Page {page}</span>
             <button onClick={nextPage}>Next</button>
-          </div> */} 
+          </div> */}
         </div>
       </div>
     </div>
