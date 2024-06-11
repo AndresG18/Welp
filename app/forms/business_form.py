a1f766d6cdccccb70591258aba45e36d7e8f9e4c
Andres Garcia <Andrese17g@gmail.com>
2024-06-11 16:27:04 -0400

    updated business backend

--- app/forms/business_form.py
+++ app/forms/business_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,IntegerField, SelectField
+from wtforms import StringField,IntegerField, SelectField,FloatField
 from wtforms.validators import DataRequired, Email, ValidationError,Length
 
 class BusinessForm(FlaskForm):
@@ -8,7 +8,10 @@ class BusinessForm(FlaskForm):
     address = StringField('Address', validators=[DataRequired(), Length(min= 0 ,max= 30)])
     city = StringField('City', validators=[DataRequired(), Length(min= 0 ,max= 20)])
     state = StringField('State', validators=[DataRequired(), Length(min= 0 ,max= 20)])
+    latitude = FloatField('Latitude', validators=[DataRequired()])
+    longitude = FloatField('Longitude', validators=[DataRequired()])
     hours = StringField('Hours', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired(), Length(min= 0 ,max= 200)])
     price = StringField('Price', validators=[DataRequired()])
     preview_image = StringField('Preview_image', validators=[DataRequired()])
+    days_open = StringField('Days of Week Open', validators=[DataRequired()])
